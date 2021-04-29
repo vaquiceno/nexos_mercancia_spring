c1e9034ed1f2b917deeac842ff3fcf62483e6954
vaquiceno <Cla3215>
2021-04-29 05:19:29 -0500

    Codigo de base de datos

---
+++ db.sql
@@ -0,0 +1,66 @@
+CREATE TABLE "cargos_nexos" (
+	"id" SERIAL,
+	"nombre" VARCHAR(20) NOT NULL,
+	PRIMARY KEY ("id")
+);
+COMMENT ON TABLE "cargos_nexos" IS 'cargos del sistema'
+;
+COMMENT ON COLUMN "cargos_nexos"."id" IS '';
+COMMENT ON COLUMN "cargos_nexos"."nombre" IS '';
+
+INSERT INTO cargos_nexos (nombre) VALUES ('Asesor Ventas');
+INSERT INTO cargos_nexos (nombre) VALUES ('Administrador');
+INSERT INTO cargos_nexos (nombre) VALUES ('Soporte');
+
+CREATE TABLE "usuarios_nexos" (
+	"id" SERIAL,
+	"nombre" VARCHAR(50) NOT NULL,
+	"edad" NUMERIC NOT NULL DEFAULT 0,
+	"cargo" SERIAL,
+	"fecha_ingreso" DATE NOT NULL DEFAULT current_date,
+	PRIMARY KEY ("id")
+);
+
+COMMENT ON TABLE "usuarios_nexos" IS 'usuarios del sistema';
+COMMENT ON COLUMN "usuarios_nexos"."id" IS E'';
+COMMENT ON COLUMN "usuarios_nexos"."nombre" IS E'';
+COMMENT ON COLUMN "usuarios_nexos"."edad" IS E'';
+COMMENT ON COLUMN "usuarios_nexos"."cargo" IS E'';
+COMMENT ON COLUMN "usuarios_nexos"."fecha_ingreso" IS E'';
+
+INSERT INTO usuarios_nexos (nombre, edad, cargo, fecha_ingreso) VALUES ('Andres Camargo', 25, 1, '02/25/2020');
+INSERT INTO usuarios_nexos (nombre, edad, cargo, fecha_ingreso) VALUES ('Felipe Marulanda', 35, 2, '02/25/2020');
+INSERT INTO usuarios_nexos (nombre, edad, cargo, fecha_ingreso) VALUES ('Horacio Angulo', 45, 3, '02/25/2020');
+
+CREATE TABLE "mercancia_nexos" (
+	"id" SERIAL NOT NULL,
+	"nombre" VARCHAR(50) NOT NULL DEFAULT E'',
+	"cantidad" INTEGER NOT NULL DEFAULT 0,
+	"fecha_ingreso" DATE NOT NULL DEFAULT CURRENT_DATE CHECK(fecha_ingreso <= CURRENT_DATE),
+	"usuario" SERIAL NOT NULL,
+	PRIMARY KEY ("id"),
+	UNIQUE ("nombre")
+);
+
+INSERT INTO mercancia_nexos(nombre,cantidad,fecha_ingreso,usuario) VALUES ('jabon', 5, '12/05/2020', 2);
+INSERT INTO mercancia_nexos(nombre,cantidad,usuario) VALUES ('cereal', 5, 2);
+
+--auditoria tabla mercancia:
+CREATE TABLE "auditoria_mercancia_nexos" (
+	"id" SERIAL NOT NULL,
+	"producto" SERIAL NOT NULL,
+	"usuario" SERIAL NOT NULL,
+	"fecha_modificacion" DATE NOT NULL DEFAULT CURRENT_DATE,
+	PRIMARY KEY ("id")
+);
+
+CREATE OR REPLACE FUNCTION proceso_mercancia_audit() RETURNS TRIGGER AS '
+    BEGIN
+        Insert into auditoria_mercancia_nexos(producto, usuario) values (NEW.id, NEW.usuario);
+        RETURN NULL; -- result is ignored since this is an AFTER trigger
+    END;
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER mercancia_audit
+AFTER UPDATE ON mercancia_nexos
+    FOR EACH ROW EXECUTE PROCEDURE proceso_mercancia_audit();
